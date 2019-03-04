27a08f5fba7b573eaef41c1b991b9eb7e78af47a
Shane Gove <shanemgove@gmail.com>
2019-03-03 19:29:54 -0600

    HTML, some CSS, some JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,96 @@
+
+$(documnet).ready(function () {
+
+//Q&A Array
+
+    var trivia = [
+        {
+            question: "What is the name of Hal Solo’s ship?"
+            choice: ["USS Enterprise", "Time Bandit", "Millennium Falcon", "Galactica"],
+            answer: 2,
+            photo: "assets/images/MFalcon.jpg"
+        },
+        {
+            question: "What actor is the voice of Woody in Toy Story?"
+            choice: ["Woody Harrelson", "Tim Allen", "Vin Diesel", "Tom Hanks"],
+            answer: 3,
+            photo: "assets/images/woody.jpg"
+        },
+        {
+            question: "Who is the most voluptuous female in Toontown?"
+            choice: ["Betty Boop", "Jessica Rabbit", "Minnie Mouse", "Wilma Flintstone"],
+            answer: 1,
+            photo: "assets/images/Rabbit.jpg"
+        },
+        {
+            question: "How many engines are on an X-wing fighter?"
+            choice: ["Four", "Eight", "Two", "One"],
+            answer: 0,
+            photo: "assets/images/xWing.gif"
+        },
+        {
+            question: "What was the name of the second Indiana Jones movie, released in 1984?"
+            choice: ["Indiana Jones and the Temple of Doom.", "Raiders of the Lost Ark", "Indiana Jones and Son", "Indiana Jones and the Last Crusade"],
+            answer: 0,
+            photo: "assets/images/temple.jpg"
+        },
+        {
+            question: "In which year were the Academy Awards, or (Oscars), first presented?"
+            choice: ["1931", "1942", "1492", "1929"],
+            answer: 3,
+            photo: "assets/images/oscars.jpg"
+        },
+        {
+            question: "Which movie of Arnold's was first nominated at the Oscars?"
+            choice: ["Total Recall", "Terminator 2", "The Running Man", "True Lies"],
+            answer: 1,
+            photo: "assets/images/terminator.gif"
+        },
+        {
+            question: "How many Die Hard movies have been released?"
+            choice: ["Four", "Five", "Six", "There is only 1 true Die Hard movie"],
+            answer: 1,
+            photo: "assets/images/dieHard.gif"
+        },
+        {
+            question: "Who directed the movie Gladiator from the year 2000?"
+            choice: ["Ridley Scott", "Steven Spielberg", "Quentin Tarantino", "David Fincher"],
+            answer: 0,
+            photo: "assets/images/gladiator.gif"
+        },
+        {
+            question: "Why didn’t Yoda train Luke Skywalker?"
+            choice: ["Yoda sensed the Darkside", "Yoda was too old", "Luke's father was Vader", "Luke lacked patience"],
+            answer: 3,
+            photo: "assets/images/Yoda.gif"
+        }
+    ];
+
+//Global variables
+
+var correct =0;
+var incorrect=0;
+var unAnswered=0;
+var timer=0;
+
+//start game when start is clicked
+
+//start timer and count down
+
+//timer function
+
+//stop timer function
+
+//reset function
+
+//load question
+
+//timeExpired function
+
+//correct answer function
+
+//incorrect answer function
+
+//reset function
+
+})
